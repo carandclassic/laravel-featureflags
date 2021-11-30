90d0a521644df0934a68208266ee8f7e1834c0ea
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2021-11-30 12:26:44 +0000

    update base

---
+++ src/ServiceProviders/MainServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CarAndClassic\LaravelFeatureFlags\ServiceProviders;
+
+use Illuminate\Support\ServiceProvider;
+
+class MainServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
