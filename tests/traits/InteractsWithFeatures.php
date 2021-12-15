7aee43f730a64924d497003f74cc74d74c93fed7
danielsimkus <daniel.simkus@carandclassic.com>
2021-12-15 15:30:47 +0000

    Add helper trait for mocking feature results

---
+++ tests/traits/InteractsWithFeatures.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CarAndClassic\LaravelFeatureFlags\Tests\traits;
+
+
+trait InteractsWithFeatures
+{
+    private function enableFeature(string $class): void
+    {
+        $this->mockFeatureResponse($class, true);
+    }
+
+    private function disableFeature(string $class): void
+    {
+        $this->mockFeatureResponse($class, false);
+    }
+
+    private function mockFeatureResponse(string $class, bool $value): void
+    {
+        $mock = $this->createMock($class);
+        $mock->expects($this->any())
+            ->method('enabled')
+            ->withAnyParameters()
+            ->willReturn($value);
+
+        app()->instance($class, $mock);
+
+    }
+}
